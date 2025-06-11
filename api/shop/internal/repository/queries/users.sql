bc167729936c6285989b01f990f42c2aa736c284
tamaco489 <tamaco489@gmail.com>
2025-06-11 22:06:54 +0900

    feature/mysql repository (#1)
    * add: setup mysql container
    
    * add: sql-migrate and sqlc queries

---
+++ api/shop/internal/repository/queries/users.sql
@@ -0,0 +1,16 @@
+-- name: CreateUser :exec
+INSERT INTO `users` (
+  `id`,
+  `username`,
+  `email`,
+  `role`,
+  `status`,
+  `last_login_at`
+) VALUES (
+  sqlc.arg('id'),
+  sqlc.arg('username'),
+  sqlc.arg('email'),
+  sqlc.arg('role'),
+  sqlc.arg('status'),
+  sqlc.arg('last_login_at')
+);
