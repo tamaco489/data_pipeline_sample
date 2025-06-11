3d8649280bfb584d3e8d958b23cb49705e9b908e
tamaco489 <tamaco489@gmail.com>
2025-06-11 22:51:27 +0900

    fix: configuration

--- api/shop/internal/configuration/configuration.go
+++ api/shop/internal/configuration/configuration.go
@@ -2,6 +2,7 @@ package configuration
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -13,10 +14,19 @@ var globalConfig Config
 type Config struct {
 	AWSConfig aws.Config
 	Logging   string `envconfig:"LOGGING" default:"off"`
-	API       struct {
+
+	API struct {
 		Env         string `envconfig:"API_ENV" default:"dev"`
 		Port        string `envconfig:"API_PORT" default:"8080"`
 		ServiceName string `envconfig:"API_SERVICE_NAME" default:"shop-api"`
+	}
+
+	CoreDB struct {
+		Host string `json:"host"`
+		Port string `json:"port"`
+		User string `json:"username"`
+		Pass string `json:"password"`
+		Name string `json:"dbname"`
 	}
 }
 
@@ -27,8 +37,19 @@ func Load(ctx context.Context) (Config, error) {
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
-	if err := loadAWSConf(ctx); err != nil {
+	env := globalConfig.API.Env
+	if err := loadAWSConf(ctx, env); err != nil {
 		return globalConfig, err
+	}
+
+	if err := batchGetSecrets(
+		ctx,
+		globalConfig.AWSConfig,
+		map[string]any{
+			fmt.Sprintf("core/%s/rds-cluster", env): &globalConfig.CoreDB,
+		},
+	); err != nil {
+		return Config{}, err
 	}
 
 	return globalConfig, nil
