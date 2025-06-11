3b0179849e26de692ca0857706a2d56ebf7e694c
tamaco489 <tamaco489@gmail.com>
2025-06-12 02:10:09 +0900

    add: migration schema tables

--- api/shop/internal/repository/migrations/20250611215221-create_users_table.sql
+++ api/shop/internal/repository/migrations/20250611215221-create_users_table.sql
@@ -1,18 +1,18 @@
 
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` VARCHAR(255) PRIMARY KEY COMMENT 'Product-specific unique ID',
-  `username` VARCHAR(255) COMMENT 'Account name in the game',
-  `email` VARCHAR(255) COMMENT 'User email address',
-  `role` ENUM('general', 'admin', 'beta_tester') NOT NULL COMMENT 'User role level',
-  `status` ENUM('active', 'inactive', 'banned') NOT NULL COMMENT 'Determine if the account is active, inactive, or banned',
-  `last_login_at` DATETIME NOT NULL COMMENT 'Last login date and time',
+  `id` VARCHAR(255) PRIMARY KEY COMMENT 'ユーザーID',
+  `username` VARCHAR(255) COMMENT 'ユーザー名',
+  `email` VARCHAR(255) COMMENT 'ユーザーのメールアドレス',
+  `role` ENUM('general', 'admin', 'beta_tester') NOT NULL COMMENT 'ユーザーの権限',
+  `status` ENUM('active', 'inactive', 'banned') NOT NULL COMMENT 'ユーザーのステータス',
+  `last_login_at` DATETIME NOT NULL COMMENT '最終ログイン日時',
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   INDEX `idx_email` (`email`),
   INDEX `idx_role` (`role`),
   INDEX `idx_status` (`status`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- +migrate Down
 DROP TABLE IF EXISTS `users`;
