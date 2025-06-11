1a167b14ef94039d1e6be97f540d489ce4515348
tamaco489 <tamaco489@gmail.com>
2025-06-12 03:11:12 +0900

    fix: migrate

--- api/shop/internal/repository/migrations/20250612020458-create_product_stocks_table.sql
+++ api/shop/internal/repository/migrations/20250612020458-create_product_stocks_table.sql
@@ -2,7 +2,7 @@
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS `product_stocks` (
   `product_id` INT UNSIGNED PRIMARY KEY COMMENT '商品ID',
-  `stock_quantity` INT NOT NULL COMMENT '在庫数',
+  `stock_quantity` INT UNSIGNED NOT NULL COMMENT '在庫数',
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (`product_id`) REFERENCES `products`(`id`)
