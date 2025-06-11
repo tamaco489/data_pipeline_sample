38e5b7e65d67a36977556cdbae5fc204ce284532
tamaco489 <tamaco489@gmail.com>
2025-06-12 04:40:22 +0900

    fix: reservations migration file

--- api/shop/internal/repository/migrations/20250612034103-create_reservations_table.sql
+++ api/shop/internal/repository/migrations/20250612034103-create_reservations_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   `user_id` VARCHAR(255) NOT NULL COMMENT '予約を行ったユーザーID',
   `product_id` INT UNSIGNED NOT NULL COMMENT '予約された商品ID',
   `reserved_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '予約日時',
+  `expired_at` DATETIME NOT NULL COMMENT '失効日時',
   `status` ENUM('pending', 'confirmed', 'canceled') NOT NULL DEFAULT 'pending' COMMENT '予約ステータス',
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
