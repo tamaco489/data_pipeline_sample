59196d06afb60f748546e5894259c73dc168873b
tamaco489 <tamaco489@gmail.com>
2025-06-13 00:23:48 +0900

    fix: di

--- api/shop/internal/usecase/create_reservation.go
+++ api/shop/internal/usecase/create_reservation.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"log/slog"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -8,11 +9,16 @@ import (
 	"github.com/tamaco489/data_pipeline_sample/api/shop/internal/gen"
 )
 
-func (u reservationUseCase) CreateReservation(ctx *gin.Context, request gen.CreateReservationRequestObject) (gen.CreateReservationResponseObject, error) {
+func (u reservationUseCase) CreateReservation(ctx *gin.Context, uid string, request gen.CreateReservationRequestObject) (gen.CreateReservationResponseObject, error) {
 
-	time.Sleep(300 * time.Millisecond)
+	time.Sleep(500 * time.Millisecond)
 
 	reservationID := uuid.New().String()
+
+	slog.InfoContext(ctx, "sample data by create reservation api",
+		"uid", uid,
+		"request", request.Body,
+		"reservation_id", reservationID)
 
 	return gen.CreateReservation201JSONResponse{
 		ReservationId: reservationID,
