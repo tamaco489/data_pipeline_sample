0dbae2df0d0079e0432afe1900e8acecd0a61324
tamaco489 <tamaco489@gmail.com>
2025-06-15 04:41:11 +0900

    fix: charges table comment

--- api/shop/internal/repository/migrations/20250612034834-create_charges_table.sql
+++ api/shop/internal/repository/migrations/20250612034834-create_charges_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `charges` (
   `user_id` VARCHAR(255) NOT NULL COMMENT '決済を行ったユーザーID',
   `amount` INT UNSIGNED NOT NULL COMMENT '請求金額（合計）',
   `status` ENUM('unpaid', 'paid', 'failed') NOT NULL DEFAULT 'unpaid' COMMENT '決済ステータス',
-  `charged_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '決済日時',
+  `charged_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '決済APIを実行し売上が確定した日時',
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (`reservation_id`) REFERENCES `reservations`(`id`),
