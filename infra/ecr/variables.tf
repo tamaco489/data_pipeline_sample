ec442087377f9170e8b950a1e88eb038ee238e69
tamaco489 <tamaco489@gmail.com>
2025-06-15 18:06:21 +0900

    add: ecr

---
+++ infra/ecr/variables.tf
@@ -0,0 +1,18 @@
+variable "env" {
+  description = "The environment in which the ecr will be created"
+  type        = string
+  default     = "stg"
+}
+
+variable "project" {
+  description = "The project name"
+  type        = string
+  default     = "fanout-architecture"
+}
+
+locals {
+  fqn                 = "${var.env}-${var.project}"
+  shop_api            = "${var.env}-shop-api"
+  slack_message_batch = "${var.env}-slack-message-batch"
+  line_message_batch  = "${var.env}-line-message-batch"
+}
