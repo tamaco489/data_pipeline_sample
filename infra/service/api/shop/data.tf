efca560f0629cf8b9781a0e81bc5d542b4ec82e3
tamaco489 <tamaco489@gmail.com>
2025-06-15 19:04:43 +0900

    add: shop api vpc lambda

---
+++ infra/service/api/shop/data.tf
@@ -0,0 +1,28 @@
+data "terraform_remote_state" "ecr" {
+  backend = "s3"
+  config = {
+    bucket = "${var.env}-data-pipeline-sample-tfstate"
+    key    = "ecr/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "acm" {
+  backend = "s3"
+  config = {
+    bucket = "${var.env}-data-pipeline-sample-tfstate"
+    key    = "acm/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "${var.env}-data-pipeline-sample-tfstate"
+    key    = "network/terraform.tfstate"
+  }
+}
+
+# AWS Managed Key
+data "aws_kms_key" "secretsmanager" {
+  key_id = "alias/aws/secretsmanager"
+}
