5fff4a82f70673800183308caf0bef9062bd6cf0
tamaco489 <tamaco489@gmail.com>
2025-06-29 17:46:16 +0900

    fix: glue data catalog

--- infra/glue/glue_catalog.tf
+++ infra/glue/glue_catalog.tf
@@ -6,7 +6,7 @@ resource "aws_glue_catalog_database" "analytics_db" {
   description = "Analytics database for data pipeline sample"
 
   parameters = {
-    classification = "parquet"
+    classification     = "parquet"
     has_encrypted_data = "false"
   }
   tags = { Name = "${local.fqn}-analytics-db" }
