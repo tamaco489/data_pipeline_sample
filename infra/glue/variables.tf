4ebff4dc1b502866c17f10b325f934582020d4d9
tamaco489 <tamaco489@gmail.com>
2025-06-29 17:10:23 +0900

    add: provider and variables

---
+++ infra/glue/variables.tf
@@ -0,0 +1,24 @@
+# =================================================================
+# general
+# =================================================================
+variable "env" {
+  description = "The environment in which the glue resources will be created"
+  type        = string
+  default     = "stg"
+}
+
+variable "project" {
+  description = "The project name"
+  type        = string
+  default     = "data-pipeline-sample"
+}
+
+variable "region" {
+  description = "The region in which the glue resources will be created"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+locals {
+  fqn = "${var.env}-${var.project}"
+}
