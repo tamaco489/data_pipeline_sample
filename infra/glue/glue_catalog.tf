f27049039029971ddc6fa2dd294a373f25fe1dba
tamaco489 <tamaco489@gmail.com>
2025-06-29 17:45:54 +0900

    add: glue data catalog

---
+++ infra/glue/glue_catalog.tf
@@ -0,0 +1,13 @@
+# =====================================================================================
+# Glue Data Catalog Database
+# =====================================================================================
+resource "aws_glue_catalog_database" "analytics_db" {
+  name        = "${local.fqn}-analytics-db"
+  description = "Analytics database for data pipeline sample"
+
+  parameters = {
+    classification = "parquet"
+    has_encrypted_data = "false"
+  }
+  tags = { Name = "${local.fqn}-analytics-db" }
+}
