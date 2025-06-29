a294fd4ec381c1fe99c808f9f85f28b148a2db5a
tamaco489 <tamaco489@gmail.com>
2025-06-29 23:52:29 +0900

    fix: todo list

--- infra/glue/outputs.tf
+++ infra/glue/outputs.tf
@@ -33,3 +33,45 @@ output "glue_crawler" {
     arn  = aws_glue_crawler.core_db.arn
   }
 }
+
+# TODO: 後続の作業で検証を兼ねて作業を行うため、一旦コメントアウト
+# # Glue Jobs
+# output "glue_jobs" {
+#   description = "Glue ETL jobs"
+#   value = {
+#     core_db_etl = {
+#       name = aws_glue_job.core_db_etl.name
+#       arn  = aws_glue_job.core_db_etl.arn
+#     }
+#     sales_analytics = {
+#       name = aws_glue_job.sales_analytics.name
+#       arn  = aws_glue_job.sales_analytics.arn
+#     }
+#   }
+# }
+
+# # EventBridge Schedulers
+# output "eventbridge_schedulers" {
+#   description = "EventBridge schedulers for Glue jobs"
+#   value = {
+#     core_db_etl = {
+#       name = aws_scheduler_schedule.core_db_etl.name
+#       arn  = aws_scheduler_schedule.core_db_etl.arn
+#     }
+#     sales_analytics = {
+#       name = aws_scheduler_schedule.sales_analytics.name
+#       arn  = aws_scheduler_schedule.sales_analytics.arn
+#     }
+#   }
+# }
+
+# # S3 Bucket References
+# output "glue_job_script_bucket" {
+#   description = "S3 bucket name for Glue job scripts"
+#   value       = data.terraform_remote_state.s3.outputs.glue_job_script.bucket
+# }
+
+# output "glue_etl_output_bucket" {
+#   description = "S3 bucket name for Glue ETL output"
+#   value       = data.terraform_remote_state.s3.outputs.glue_etl_output.bucket
+# }
